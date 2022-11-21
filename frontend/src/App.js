379a87188c816b1a85bf72b49435e297d276c0c0
Clemente Nogueira <git.3gear@gmail.com>
2022-11-21 21:44:26 +0000

    The frontend of the project has been created.

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <a href="/">TheStore</a>
+      </header>
+        <main>
+            list products
+        </main>
+    </div>
+  );
+}
+
+export default App;
