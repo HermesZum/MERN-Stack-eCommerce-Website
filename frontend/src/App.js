5688b70430d92a43354723aa1bb293579b27b3e0
Clemente Nogueira <git.3gear@gmail.com>
2022-11-21 22:34:13 +0000

    The featured products page was created and configured.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from "./data/data";
 
 function App() {
   return (
@@ -7,7 +8,25 @@ function App() {
           <a href="/">TheStore</a>
       </header>
         <main>
-            list products
+            <h1>Featured Products</h1>
+            <div className="products">
+                {
+                    data.products.map(product => (
+                        <div  className="product" key={ product.slug }>
+                            <a href={`/product/${product.slug}`}>
+                            <img src={ product.image } alt={ product.name }/>
+                            </a>
+                            <div className="product-info">
+                                <a href={`/product/${product.slug}`}>
+                                    <p>{ product.name }</p>
+                                </a>
+                                <p><strong>${ product.price }</strong></p>
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </main>
     </div>
   );
