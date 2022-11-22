245b34b1f0be6ef4e5fff91c0bab5303191fce96
Clemente Nogueira <git.3gear@gmail.com>
2022-11-22 15:10:33 +0000

    Home and Product screens were created and configured.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
 import './App.css';
-import data from "./data/data";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <a href="/">TheStore</a>
-      </header>
-        <main>
-            <h1>Featured Products</h1>
-            <div className="products">
-                {
-                    data.products.map(product => (
-                        <div  className="product" key={ product.slug }>
-                            <a href={`/product/${product.slug}`}>
-                            <img src={ product.image } alt={ product.name }/>
-                            </a>
-                            <div className="product-info">
-                                <a href={`/product/${product.slug}`}>
-                                    <p>{ product.name }</p>
-                                </a>
-                                <p><strong>${ product.price }</strong></p>
-                                <button>Add to cart</button>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-        </main>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+              <Link to="/">TheStore</Link>
+          </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<HomeScreen />} />
+                    <Route path="/product/:slug" element={<ProductScreen />} />
+                </Routes>
+            </main>
+        </div>
+      </BrowserRouter>
   );
 }
 
