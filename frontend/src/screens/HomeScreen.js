99991cbd505842735d8b6855ba2e11af9c0aae7d
Clemente Nogueira <git.3gear@gmail.com>
2022-11-22 16:14:39 +0000

    Home and Product screens were created and configured.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import data from "../data/data";
+// import data from "../data/data";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomeScreen() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get('/api/products');
+            setProducts(result.data);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h1>Featured Products</h1>
             <div className="products">
                 {
-                    data.products.map(product => (
+                    products.map(product => (
                         <div  className="product" key={ product.slug }>
                             <Link to={`/product/${product.slug}`} >
                                 <img src={ product.image } alt={ product.name }/>
