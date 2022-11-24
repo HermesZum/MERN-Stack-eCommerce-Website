dfa4baeb58d0bf7ba6835694e65b3054d1a12bcf
Clemente Nogueira <git.3gear@gmail.com>
2022-11-24 19:35:30 +0000

    The mechanics of the Store and shopping cart were created and initially configured.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import { Container, Navbar } from "react-bootstrap";
+import { Badge, Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 function App() {
+    const { state } = useContext(Store);
+    const { cart } = state;
   return (
       <BrowserRouter>
         <div className="d-flex flex-column site-container">
@@ -17,6 +21,17 @@ function App() {
                             TheStore
                         </Navbar.Brand>
                     </LinkContainer>
+                    <Nav className="me-auto">
+                        <Link to="/cart" className="nav-link">
+                            <i className={ 'fas fa-cart-shopping' }
+                            />
+                            { cart.cartItems.length > 0 && (
+                                <Badge pill bg="danger">
+                                    { cart.cartItems.length }
+                                </Badge>
+                            )}
+                        </Link>
+                    </Nav>
                 </Container>
               </Navbar>
           </header>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { Badge, Button, Card, Col, ListGroup, Row } from "react-bootstrap";
@@ -7,6 +7,7 @@ import Rating from "../components/Rating";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
+import { Store } from "../Store";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -44,6 +45,15 @@ function ProductScreen() {
         };
         fetchData();
     }, [slug]);
+
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+
+    const addToCartHandler = () => {
+        ctxDispatch({
+                        type: 'CART_ADD_ITEM',
+                        payload: { ...product, quantity: 1 },
+                    });
+    }
 
     return loading
         ? <LoadingBox />
@@ -102,7 +112,7 @@ function ProductScreen() {
                                     product.countInStock > 0 && (
                                         <ListGroup.Item>
                                             <div className="d-grid">
-                                                <Button variant="primary">
+                                                <Button onClick={ addToCartHandler } variant="primary">
                                                     Add to Cart
                                                 </Button>
                                             </div>
