05950aa3070168c3e0c485464834827c32292514
Clemente Nogueira <git.3gear@gmail.com>
2022-11-25 20:44:39 +0000

    The Cart screen and its mechanics have been improved.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,33 @@
 import { Link } from "react-router-dom";
 import { Button, Card } from "react-bootstrap";
 import Rating from "./Rating";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../Store";
 
 function Product(props) {
     const { product } = props;
+
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const  {
+        cart: { cartItems },
+    } = state;
+
+    const addToCartHandler = async (item) => {
+        const existItem = cartItems.find((x) => x._id === product._id);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+        const { data } = await axios.get(`/api/products/${ item._id }`);
+
+        if (data.countInStock < quantity) {
+            window.alert('Sorry. Product is out of stock');
+            return;
+        }
+
+        ctxDispatch({
+                        type: 'CART_ADD_ITEM',
+                        payload: { ...item, quantity },
+                    });
+    };
 
     return (
         <Card>
@@ -18,7 +42,21 @@ function Product(props) {
                 <Card.Text>
                     ${ product.price }
                 </Card.Text>
-                <Button>Add to cart</Button>
+                { product.countInStock === 0
+                    ?
+                    <Button
+                        variant="light"
+                        disabled
+                    >
+                        Out of stock
+                    </Button>
+                    :
+                    <Button
+                        onClick={() => addToCartHandler(product)}
+                    >
+                        Add to cart
+                    </Button>
+                }
             </Card.Body>
         </Card>
     );
