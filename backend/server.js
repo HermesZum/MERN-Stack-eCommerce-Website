5a7dc272051d8cfa2336f90a2e7ad30442251110
Clemente Nogueira <git.3gear@gmail.com>
2022-11-27 01:46:08 +0000

    An API for user sign in was created.

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,32 @@ import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
-    console.log('Connected to DB!');
-})
+            console.log('Connected to DB!');
+        })
         .catch((e) => {
-    console.log(e.message);
-});
+            console.log(e.message);
+        });
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
-const HOST = process.env.HOST || "localhost"
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
+
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
