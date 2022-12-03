0c32695c6c7bd85364c0f51e4b24975139741a08
Clemente Nogueira <git.3gear@gmail.com>
2022-12-03 23:10:42 +0000

    The Order History screen and its logic were created and configured.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 import OrderScreen from "./screens/OrderScreen";
 
 function App() {
@@ -32,52 +33,55 @@ function App() {
             <div className="d-flex flex-column site-container">
                 <ToastContainer position="top-center" limit={ 1 }/>
                 <header>
-                    <Navbar bg="dark" variant="dark">
+                    <Navbar bg="dark" variant="dark" expand="lg">
                         <Container>
                             <LinkContainer to="/">
                                 <Navbar.Brand>
                                     TheStore
                                 </Navbar.Brand>
                             </LinkContainer>
-                            <Nav className="me-auto">
-                                <Link to="/cart" className="nav-link">
-                                    <i className={ 'fas fa-cart-shopping' }
-                                    />
-                                    { cart.cartItems.length > 0 && (
-                                        <Badge pill bg="danger">
-                                            { cart.cartItems.reduce((a, c) => a + c.quantity, 0) }
-                                        </Badge>
-                                    ) }
-                                </Link>
-                                {
-                                    userInfo
-                                    ?
-                                    (
-                                        <NavDropdown title={ userInfo.username } id="basic-nav-dropdown">
-                                            <LinkContainer to="/profile">
-                                                <NavDropdown.Item>User Profile</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <LinkContainer to="/orderhistory">
-                                                <NavDropdown.Item>Order History</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <NavDropdown.Divider/>
-                                            <Link
-                                                className="dropdown-item"
-                                                to="#signout"
-                                                onClick={ signoutHandler }
-                                            >
-                                                Sign Out
+                            <Navbar.Toggle aria-controls="basic-navbar-nav "/>
+                            <Navbar.Collapse id="basic-navbar-nav ">
+                                <Nav className="me-auto w-100 justify-content-end">
+                                    <Link to="/cart" className="nav-link">
+                                        <i className={ 'fas fa-cart-shopping' }
+                                        />
+                                        { cart.cartItems.length > 0 && (
+                                            <Badge pill bg="danger">
+                                                { cart.cartItems.reduce((a, c) => a + c.quantity, 0) }
+                                            </Badge>
+                                        ) }
+                                    </Link>
+                                    {
+                                        userInfo
+                                        ?
+                                        (
+                                            <NavDropdown title={ userInfo.username } id="basic-nav-dropdown">
+                                                <LinkContainer to="/profile">
+                                                    <NavDropdown.Item>User Profile</NavDropdown.Item>
+                                                </LinkContainer>
+                                                <LinkContainer to="/orderhistory">
+                                                    <NavDropdown.Item>Order History</NavDropdown.Item>
+                                                </LinkContainer>
+                                                <NavDropdown.Divider/>
+                                                <Link
+                                                    className="dropdown-item"
+                                                    to="#signout"
+                                                    onClick={ signoutHandler }
+                                                >
+                                                    Sign Out
+                                                </Link>
+                                            </NavDropdown>
+                                        )
+                                        :
+                                        (
+                                            <Link className="nav-link" to="/signin">
+                                                Sign In
                                             </Link>
-                                        </NavDropdown>
-                                    )
-                                    :
-                                    (
-                                        <Link className="nav-link" to="/signin">
-                                            Sign In
-                                        </Link>
-                                    )
-                                }
-                            </Nav>
+                                        )
+                                    }
+                                </Nav>
+                            </Navbar.Collapse>
                         </Container>
                     </Navbar>
                 </header>
@@ -92,6 +96,7 @@ function App() {
                             <Route path="/shipping" element={ <ShippingScreen/> }/>
                             <Route path="/payment" element={ <PaymentMethodScreen/> }/>
                             <Route path="/placeorder" element={ <PlaceOrderScreen/> }/>
+                            <Route path="/orderhistory" element={ <OrderHistoryScreen/> }/>
                             <Route path="/order/:id" element={ <OrderScreen/> }/>
                         </Routes>
                     </Container>
---
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { useEffect, useContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { getError } from "../utils";
+import { Button } from "react-bootstrap";
+import { Store } from "../Store";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, orders: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+
+        default:
+            return state;
+    }
+};
+
+function OrderHistoryScreen(props) {
+    const { state } = useContext(Store);
+    const { userInfo } = state;
+    const navigate = useNavigate();
+
+    const [ { loading, error, orders }, dispatch ] = useReducer(reducer, {
+        loading: true,
+        error: '',
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const { data } = await axios.get(`/api/orders/mine`,
+                    { headers: { Authorization: `Bearer ${ userInfo.token }` } }
+                );
+                dispatch({ type: 'FETCH_SUCCESS', payload: data });
+            }
+            catch (err) {
+                dispatch({
+                    type: 'FETCH_FAIL',
+                    payload: getError(err),
+                });
+            }
+        };
+        fetchData();
+    }, [ userInfo ]);
+
+
+    return (
+        <div>
+            <Helmet>
+                <title>Order History</title>
+            </Helmet>
+            <h1>order History</h1>
+            { loading
+              ? (<LoadingBox></LoadingBox>)
+              : error
+                ? (<MessageBox variant="danger">{ error }</MessageBox>)
+                : (
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>DATE</th>
+                            <th>TOTAL</th>
+                            <th>PAID</th>
+                            <th>DELIVERED</th>
+                            <th>ACTIONS</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        { orders.map((order) => (
+                            <tr key={ order._id }>
+                                <td>{ order._id }</td>
+                                <td>{ order.createdAt.substring(0, 10) }</td>
+                                <td>${ order.totalPrice.toFixed(2) }</td>
+                                <td>{ order.isPaid ? order.paidAt.substring(0, 10) : 'No' }</td>
+                                <td>{ order.isDelivered ? order.deliveredAt.substring(0, 10) : 'No' }</td>
+                                <td>
+                                    <Button
+                                        type="button"
+                                        variant="light"
+                                        onClick={ () => { navigate(`/order/${ order._id }`);} }
+                                    >
+                                        Details
+                                    </Button>
+                                </td>
+                            </tr>
+                        )) }
+                        </tbody>
+                    </table>
+                ) }
+        </div>
+    );
+}
+
+export default OrderHistoryScreen;
